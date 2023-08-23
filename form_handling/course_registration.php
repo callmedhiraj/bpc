1accf2bd125608c1a5139bf77165bf2c36dacb1f
Dhiraj Kumar Jha <callmedhiraj@gmail.com>
2023-08-23 19:57:15 +0545

    day5 status

--- form_handling/course_registration.php
+++ form_handling/course_registration.php
@@ -3,16 +3,58 @@
         <title>Course Registration Form </title>
 </head>
 <body>
-    <form name="course_registration" id="course_registration" method="post" action="process_course_registration.php">
+    <form name="course_registration" id="course_registration" 
+    method="post" action="process_course_registration.php"
+    enctype="multipart/form-data" >
 <div id="txtName" > <label> Name </label> : 
  <input type="text" name="txt_name" id="txt_name" placeholder="Enter your name" />
+    
+</div>
+<div id="texPhone" > <label> Phone </label> : 
+ <input type="number" name="txtPhone" id="txtPhone" placeholder="Enter your Phone" />
     
 </div>
 <div id="txtPassword" > <label> Password </label> : 
  <input type="password" name="txtPassword" id="txtPassword" placeholder="Enter your password" />
     
 </div>
-<div>    
+<div id="txtEmail" > <label> Email </label> : 
+ <input type="email" name="txtEmail" id="txtEmail" placeholder="Enter your Email" />
+    
+</div>
+<div id="txtAreaEnquiry" > <label> Enquiry </label> : 
+<textarea name="txtAreaEnquiry" id="txtAreaEnquiry"></textarea>
+    
+</div>
+<div><!-- grouping checkboxes -->
+<input type="checkbox" name="courses[]" id="chkBIM" value="bim" /> BIM 
+<input type="checkbox" name="courses[]" id="chkCSIT" value="csit" /> BScCSIT 
+<input type="checkbox" name="courses[]" id="chkBCA" value="bca" /> BCA 
+</div>
+<div> <lable>Gender </lable> <!-- group of RB -->
+<input type="radio" name="gender" value="male" checked />Male
+<input type="radio" name="gender" value="female"  /> Female
+<input type="radio" name="gender" value="others" checked /> Others
+</div>
+<div>
+    <label> Upload your Image </label>
+    <input type="file" name="profile_pic"id="profile_pic"  />
+</div>
+
+<div>
+    <label>City</label>
+    <select name="city"id="city">
+        <option value="pokhara" >Pokhara</option>
+        <option value="biratnagar" >Biratnagar</option>
+        <option value="rajbiraj" >Rajbiraj</option>
+        <option value="birgunj" selected >Birgunj</option>
+        <option value="hetauda" >Hetauda</option>
+        <option value="nepalgunj" >Nepalgunj</option>
+
+    </select>
+</div>
+<div>  
+<input type="hidden" name="hidden" value="104" />
 <input type="submit" name="submit" id="submit" value="SUBMIT" />
 </div>
 </form>
--- form_handling/process_course_registration.php
+++ form_handling/process_course_registration.php
@@ -6,7 +6,37 @@ echo '<br />'.$_SERVER['REQUEST_URI'] ;
 $name = $_POST['txt_name']; // RAW - vulnerable
 $txtPassword = sha1($_POST['txtPassword']);
 //$_REQUEST /// not recommended at all
+//to recieve the hidden value
+$hidden = $_POST['hidden'];
+//group of check box
+$courses = $_POST['courses'];
+
+echo '<pre>';
+print_r($courses);
+echo '</pre>';
+//convert array to string
+$str = implode(',',$courses);
 
 echo "Welcome Dear, " . $name;
-echo "<br /> Your password is ".$txtPassword;  
+echo "<br /> Hidden value is ". $hidden;  
+echo "<br /> The courses chosen are ". $str;  
+
+$sql = "INSERT INTO tbl_enqiries VALUES () ";
+
+///dealing with files : 2D Arrays
+echo '<pre>';
+print_r($_FILES);
+echo '</pre>';
+
+
+
+$destination='../uploads/'.date('yyyy_mm_dd_hh_ii_ss_').$_FILES['profile_pic']['name'];
+if(move_uploaded_file($_FILES['profile_pic']['tmp_name'], $destination)){
+    echo "The file has been uploaded ";
+}else{
+    echo "The file could not be uploaded";
+}
+
+///
+$sql  = 'INSERT INTO tbl_name VALUES ('.$destination.')';
 ?>
